329539468172e30cf28f4a02a93d5d09c02b361f
plantimals <5475390+plantimals@users.noreply.github.com>
2021-01-25 19:53:53 -0600

    hold

--- main.go
+++ main.go
@@ -8,31 +8,28 @@ import (
 )
 
 type Enclosure struct {
-	url    string `json:"@url"`
-	length string `json:"@length"`
-	etype  string `json:"@type"`
+	Url    string `json:"@url"`
+	Length string `json:"@length"`
+	Etype  string `json:"@type"`
 }
 
 type PodcastEpisode struct {
-	title       string    `json:"title"`
-	enclosure   Enclosure `json:"enclosure"`
-	description string    `json:"description"`
-	link        string    `json:"link"`
+	Title       string `json:"title"`
+	En          Enclosure
+	Description string `json:"description"`
+	Link        string `json:"link"`
 }
 
 func main() {
-
 	input, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(input))
 	var pce PodcastEpisode
 	err = json.Unmarshal(input, &pce)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(pce)
 	pp, err := json.MarshalIndent(pce, "", "\t")
 	if err != nil {
 		panic(err)
