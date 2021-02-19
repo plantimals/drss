d2d280e46a8d8f42f49569abaecf82d0decb6c82
plantimals <5475390+plantimals@users.noreply.github.com>
2021-02-18 18:29:28 -0600

    semi-function

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 
+	"log"
+
+	shell "github.com/ipfs/go-ipfs-api"
 	"github.com/mmcdole/gofeed"
 )
 
@@ -17,20 +24,24 @@ type Config struct {
 	FeedURL     string
 }
 
+type Link struct {
+	addr string
+}
+
+func (l *Link) toJSON() string {
+	return fmt.Sprintf("{\"/\":\"%s\"", l.addr)
+}
+
+type IPFeed struct {
+	item string `json:"item"`
+}
+
 func parseFlags() *Config {
 	var storagePath string
 	var feedURL string
 	flag.StringVar(&storagePath, "storage", "./feed", "path to construct feed")
 	flag.StringVar(&feedURL, "feedURL", "https://feeds.transistor.fm/the-vance-crowe-podcast", "feed URL")
 	flag.Parse()
-
-	/*s, err := os.Stat(storagePath)
-	if err != nil {
-		panic(err)
-	}
-	if s.IsDir() {
-		panic(err)
-	}*/
 
 	_, err := url.ParseRequestURI(feedURL)
 	if err != nil {
@@ -49,17 +60,36 @@ func rssToISS(config *Config) error {
 	if err != nil {
 		panic(err)
 	}
-	fn, err := os.Stat(config.StoragePath)
-	if fn != nil {
-		panic(fmt.Errorf("there's already a directory at %s\n", config.StoragePath))
-	}
-	err = os.Mkdir(config.StoragePath, 0700)
+	s := shell.NewShell("localhost:5001")
+
+	var itemNodes []string
+	//for _, i := range feed.Items {
+	itemNodes = append(itemNodes, getItemNode(feed.Items[0], s))
+	//itemNodes = append(itemNodes, getItemNode(feed.Items[1], s))
+	//}
+	fmt.Println(getFeedNode(itemNodes, s))
+	return nil
+}
+
+func getFeedNode(f *gofeed.Feed, items []string, s *shell.Shell) string {
+
+}
+
+func getItemNode(i *gofeed.Item, s *shell.Shell) string {
+	ib, err := getItemJSON(i)
+	c, err := s.DagPut(ib, "json", "cbor")
 	if err != nil {
 		panic(err)
 	}
-	addEpisode(feed.Items[0], fmt.Sprintf("%s/%s", config.StoragePath, "0"))
-	publishToIPFS(config)
-	return nil
+	return strings.ToUpper(c)
+}
+
+func getItemJSON(i *gofeed.Item) ([]byte, error) {
+	answer, err := json.Marshal(i)
+	if err != nil {
+		return nil, err
+	}
+	return answer, nil
 }
 
 func getFeed(url string) (*gofeed.Feed, error) {
@@ -71,6 +101,7 @@ func getFeed(url string) (*gofeed.Feed, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return feed, nil
 }
 
@@ -80,10 +111,25 @@ func addEpisode(episode *gofeed.Item, path string) {
 	if err != nil {
 		panic(err)
 	}
+
+	//addText(path, "original-json", episode.Extensions)
 	addText(path, "title", episode.Title)
+	addText(path, "description", episode.Description)
+	addText(path, "published", episode.Published)
+	addText(path, "author", episode.Author.Name)
+	addText(path, "link", episode.Link)
+	addText(path, "summary", episode.Custom["itunes:summary"])
+	addText(path, "explicit", episode.Custom["itunes:explicit"])
+	addText(path, "keywords", episode.Custom["itunes:keywords"])
+	addFile(path, "episode.mp3", episode.Enclosures[0].URL)
+	addFile(path, "image.jpg", episode.Image.URL)
 }
 
 func addText(path string, key string, value string) {
+	if len(value) == 0 {
+		log.Print("no value for key: ", key)
+		return
+	}
 	f, err := os.Create(fmt.Sprintf("%s/%s", path, key))
 	if err != nil {
 		panic(err)
@@ -96,7 +142,22 @@ func addText(path string, key string, value string) {
 }
 
 func addFile(path string, key string, url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
 
+	out, err := os.Create(fmt.Sprintf("%s/%s", path, key))
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func publishToIPFS(config *Config) {
---
+++ up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+def main():
+    str = "bafyreibgquj3d2m4lztsiliikvlyozpt4r7yfx5onvbntqwatcasuqq4hm"
+    print str.upper()
+
+if __name__ == "__main__":
+    main()
